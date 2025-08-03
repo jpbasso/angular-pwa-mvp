d15c7da402e28b5be5cd633b57aeeb5ace795be5
Juan Pablo Basso <41083282+jpbasso@users.noreply.github.com>
2025-08-03 19:50:27 -0300

    Configure PrimeNG theme via provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,6 +10,8 @@ import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeuix/themes/lara';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,6 +20,11 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: Lara,
+      },
+    }),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
